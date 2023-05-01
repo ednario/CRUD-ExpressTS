cbf60599af88d876be386b23da8d92e40fd673f4
Ednario Andrade <joseednario637@gmail.com>
2023-05-01 17:12:56 -0300

    chore: create search route of all categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { CreateCategoryController } from "../controllers/CreateCategoryController";
+import { GetAllCategoryController } from "../controllers/GetAllCategoriesController";
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", new CreateCategoryController().handle);
+categoriesRoutes.get("/", new GetAllCategoryController().handle);
 
 export { categoriesRoutes };
