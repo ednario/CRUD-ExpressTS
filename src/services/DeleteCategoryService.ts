917c7ffe6c9282121254247179b4b1422d72ad8b
Ednario Andrade <joseednario637@gmail.com>
2023-05-01 17:13:37 -0300

    chore: create category delete service

---
+++ src/services/DeleteCategoryService.ts
@@ -0,0 +1,14 @@
+import { getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+
+export class DeleteCategoryService {
+  async execute(id: string): Promise<void | Error> {
+    const repo = getRepository(Category);
+
+    if (!(await repo.findOne({ id }))) {
+      return new Error("Categories does not exists!");
+    }
+
+    await repo.delete(id);
+  }
+}
