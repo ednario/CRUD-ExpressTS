44b060740b1f8fe509b436b274bdcba62959bd33
Ednario Andrade <joseednario637@gmail.com>
2023-05-01 18:19:46 -0300

    feat: ensure the category is updated

---
+++ src/controllers/UpdateCategoryController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+
+import { UpdateCategoryService } from "../services/UpdateCategoryService";
+
+export class UpdateCategoryController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+    const { name, description } = request.body;
+
+    const updateCategoryService = new UpdateCategoryService();
+
+    const result = await updateCategoryService.execute({
+      id,
+      name,
+      description,
+    });
+
+    if (result instanceof Error) {
+      return response.status(400);
+    }
+
+    return response.json(result);
+  }
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 
 import { CreateCategoryController } from "../controllers/CreateCategoryController";
 import { GetAllCategoryController } from "../controllers/GetAllCategoriesController";
+import { DeleteCategoryController } from "../controllers/DeleteCategoryController";
+import { UpdateCategoryController } from "../controllers/UpdateCategoryController";
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", new CreateCategoryController().handle);
 categoriesRoutes.get("/", new GetAllCategoryController().handle);
+categoriesRoutes.delete("/:id", new DeleteCategoryController().handle);
+categoriesRoutes.put("/:id", new UpdateCategoryController().handle);
 
 export { categoriesRoutes };
