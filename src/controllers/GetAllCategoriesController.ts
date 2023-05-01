8a6feb679eb1fae18d334b2b5ff712097d211731
Ednario Andrade <joseednario637@gmail.com>
2023-05-01 17:11:53 -0300

    chore: reate search controller for all categories

---
+++ src/controllers/GetAllCategoriesController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+import { GetAllCategoriesService } from "../services/GetAllCategoriesService";
+
+export class GetAllCategoryController {
+  async handle(request: Request, response: Response) {
+    const getAllCategoryService = new GetAllCategoriesService();
+
+    const result = await getAllCategoryService.execute();
+
+    return response.json(result);
+  }
+}
