21dbd02ec6786e92fd7660fcb2dc94e287a3d992
Ednario Andrade <joseednario637@gmail.com>
2023-05-01 14:47:33 -0300

    chore: create videos migration

---
+++ src/database/migrations/1682963236863-CreateVideos.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateVideos1682963236863 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
